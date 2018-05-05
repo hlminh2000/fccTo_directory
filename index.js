b4fc3655b1c0791ae9af3bb728525fa2fbbfd328
Adam Rogers <cldd5678@hotmail.com>
2018-05-05 16:01:03 -0400

    Add a comma that definitely wasn't missed

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const users = [
     bio:
       "⚛️ Senior JS Developer @TelusDigital || @FreeCodeCamp core || Working on my own garden. Cannot care if your side of the hedge seems greener. || INTJ-A Úlfhéðin",
     github: "@hallaathrad"
-  }
+  },
   {
     name: "Adam Rogers",
     bio: "Professional wizard. Harry Potter has nothing on me.",
