39aa5c897aaef41154a6259cb1b207c89109fcf6
Erkand Imeri <erkand.imeri@gmail.com>
2018-05-05 15:57:23 -0400

    first commit

--- index.js
+++ index.js
@@ -20,6 +20,12 @@ const users = [
     bio:
       "⚛️ Senior JS Developer @TelusDigital || @FreeCodeCamp core || Working on my own garden. Cannot care if your side of the hedge seems greener. || INTJ-A Úlfhéðin",
     github: "@hallaathrad"
+  },
+  {
+    name: "Erkand Imeri",
+    bio:
+      "⚛️ Full Stack Developer @SKAB || @FreeCodeCamp || Hiking, Reading, Jogging.",
+    github: "@erkand-imeri"
   }
 ];
 
