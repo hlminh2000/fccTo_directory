920ccb959a8cba64329f9eb8e25f784fa0356e1c
Sylvain Junca <sylvain@Sylvains-MacBook-Pro.local>
2018-05-05 15:54:33 -0400

    Add the canneles baker to the team

--- index.js
+++ index.js
@@ -20,6 +20,11 @@ const users = [
     bio:
       "⚛️ Senior JS Developer @TelusDigital || @FreeCodeCamp core || Working on my own garden. Cannot care if your side of the hedge seems greener. || INTJ-A Úlfhéðin",
     github: "@hallaathrad"
+  },
+  {
+    name: "Sylvain Junca",
+    bio: "I am canelés baker and I want to become a Developer",
+    github: "@SylvainJunca",
   }
 ];
 
